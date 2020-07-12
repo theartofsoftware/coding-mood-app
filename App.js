4049a446720c36a79fe833e9477c657b59aaa9f1
Mikey Mo <mikeyjkmo@gmail.com>
2020-07-12 18:53:33 +0100

    Flashing red light with ominous music

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
+import pianoHorror from './assets/mixkit-piano-horror-671.mp3';
+
+const soundObject = new Audio.Sound();
+
+const FlashingView = (props) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    const flash = () => {
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(
+            fadeAnim,
+            {
+              toValue: 1,
+              duration: 1000,
+              useNativeDriver: false,
+            }
+          ),
+          Animated.timing(
+            fadeAnim,
+            {
+              toValue: 0,
+              duration: 1000,
+              useNativeDriver: false,
+            }
+          ),
+        ])
+      ).start();
+    };
+
+    flash();
+  }, [fadeAnim])
+
+  return (
+    <Animated.View
+      style={{
+        ...props.style,
+        opacity: fadeAnim,
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
 
 export default function App() {
+  useEffect(() => {
+    const playSound = async () => {
+      try {
+        await soundObject.loadAsync(pianoHorror);
+        await soundObject.playAsync();
+        // Your sound is playing!
+      } catch (error) {
+        // An error occurred!
+        console.log(error);
+      }
+    };
+
+    playSound();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlashingView style={styles.animated}>
+      </FlashingView>
     </View>
   );
 }
@@ -14,7 +75,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  animated: {
+    flex: 1,
+    backgroundColor: '#f00',
     alignItems: 'center',
     justifyContent: 'center',
   },
