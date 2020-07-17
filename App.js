3493df581a4fc0a993ee35cb25bebd501f072f4b
Mikey Mo <mikeyjkmo@gmail.com>
2020-07-17 20:19:15 +0100

    Move logic inside MoodController

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Audio } from 'expo-av';
 import SoundPlayer from './SoundPlayer';
@@ -6,36 +6,16 @@ import FlashingView from './FlashingView';
 import MoodController from './MoodController';
 
 const soundPlayer = new SoundPlayer(Audio);
-const moodController = new MoodController("ws://192.168.1.189:5000/ws");
-
+const moodController = new MoodController("ws://192.168.1.189:5000/ws", soundPlayer);
 
 export default function App() {
   const [flashColor, setFlashColor] = useState('transparent');
 
-  const toggleMode = useCallback(async () => {
-    const newFlashColor = (flashColor === "transparent") ? "#f00" : "transparent";
-    setFlashColor(newFlashColor);
-    await soundPlayer.toggleSong();
-  }, [flashColor]);
-
   useEffect(() => {
-    const startSounds = async () => {
-      await soundPlayer.init();
-      await soundPlayer.start();
-      moodController.connect();
-    };
-
-    startSounds();
-
-    return async () => {
-      await soundPlayer.stop();
-      moodController.close();
-    };
+    moodController.setFlashColorCb(setFlashColor);
+    moodController.start();
+    return () => moodController.stop();
   }, []);
-
-  useEffect(() => {
-    moodController.setToggleModeCallback(toggleMode);
-  }, [toggleMode])
 
   return (
     <View style={styles.container}>
--- MoodController.js
+++ MoodController.js
@@ -1,34 +1,51 @@
 class MoodController {
-  constructor(url, reconnectDelay=1000) {
+  constructor(url, soundPlayer, reconnectDelay=1000) {
     this.url = url;
     this.ws = null;
-    this.toggleMode = null;
+    this.closed = false;
+    this.setFlashColor = () => {};
+
+    this.soundPlayer = soundPlayer;
     this.reconnectDelay = reconnectDelay;
+  }
+
+  setFlashColorCb(setFlashColor) {
+    this.setFlashColor = setFlashColor;
+  }
+
+  async start() {
+    await this.soundPlayer.init();
+    await this.soundPlayer.start();
+    this.connect();
   }
 
   connect() {
     this.ws = new WebSocket(this.url);
     this.ws.onopen = () => console.log("Websocket opened");
     this.ws.onclose = () => {
-      console.log("Websocket closed. Will try to re-connect");
-      setTimeout(() => this.connect(), this.reconnectDelay);
+      if (!this.closed) {
+        console.log("Websocket closed. Will try to re-connect");
+        setTimeout(() => this.connect(), this.reconnectDelay);
+      }
     };
     this.ws.onmessage = e => this.onMessage(e.data);
   }
 
-  setToggleModeCallback(toggleMode) {
-    this.toggleMode = toggleMode;
-  }
+  async onMessage(data) {
+    const { state } = JSON.parse(data);
 
-  onMessage(data) {
-    const parsedData = JSON.parse(data);
-    if (parsedData.code_state === "working" && this.toggleMode) {
-      this.toggleMode();
+    if (state === "working") {
+      this.setFlashColor("#000");
+      await this.soundPlayer.toggleSong();
+    } else if (state === "broken") {
+      this.setFlashColor("#f00");
+      await this.soundPlayer.toggleSong();
     }
   }
 
-  close() {
+  stop() {
     if (this.ws) {
+      this.closed = true;
       this.ws.close();
     }
   }
